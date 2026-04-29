b78c0ba9a792567048abad8ecc879ef1381f9110
Eyub197 <eyubmehmed122@gmail.com>
2026-04-29 09:09:49 +0300

    assets and metadata

--- css-for-js-devs/workshops/4-sole-and-ankle-v2/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ css-for-js-devs/workshops/4-sole-and-ankle-v2/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
 const Breadcrumbs = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
+	return <Wrapper>{children}</Wrapper>;
 };
 
 Breadcrumbs.Crumb = ({ href, children, delegated }) => {
-  return (
-    <CrumbWrapper>
-      <CrumbLink href={href} {...delegated}>
-        {children}
-      </CrumbLink>
-    </CrumbWrapper>
-  );
+	return (
+		<CrumbWrapper>
+			<CrumbLink href={href} {...delegated}>
+				{children}
+			</CrumbLink>
+		</CrumbWrapper>
+	);
 };
 
 const CrumbWrapper = styled.div`
--- js-mastery-pro/ultimate-nextjs-course/app/page.tsx
+++ js-mastery-pro/ultimate-nextjs-course/app/page.tsx
@@ -1,9 +1,12 @@
 function Page() {
-	return (
-		<main>
-			<h1 className="text-5xl">Welcome to the Ultimate Next.js course!</h1>
-		</main>
-	);
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the Ultimate Next.js course!</h1>
+      <p className="font-space-grotesk">
+        hello I dont really like this course{" "}
+      </p>
+    </>
+  );
 }
 
 export default Page;
