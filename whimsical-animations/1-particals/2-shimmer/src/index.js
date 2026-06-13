d1726ead5558f8d44d387195601dca39128eda6d
Eyub197 <eyubmehmed122@gmail.com>
2026-06-13 10:02:22 +0300

    Second exercise

---
+++ whimsical-animations/1-particals/2-shimmer/src/index.js
@@ -0,0 +1,29 @@
+import "./reset.css";
+import "./styles.css";
+
+const btn = document.querySelector(".buyButton");
+const SHIMMER_DURATION = 1000;
+
+function generateShimmer() {
+	const shimmer = document.createElement("span");
+	shimmer.classList.add("shimmer");
+	shimmer.style.animationDuration = `${SHIMMER_DURATION}ms`;
+
+	btn.appendChild(shimmer);
+
+	setTimeout(() => {
+		shimmer.remove();
+	}, SHIMMER_DURATION);
+}
+
+btn.addEventListener("mouseenter", generateShimmer);
+btn.addEventListener("focus", generateShimmer);
+
+/*
+  DOM manipulation cheatsheet:
+
+  - Set an inline style:
+    `element.style.backgroundColor = "white";`
+  - Destroy an element:
+    `element.remove();`
+*/
