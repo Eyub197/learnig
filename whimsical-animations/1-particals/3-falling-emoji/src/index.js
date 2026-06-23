d6feb5b6aa5731aea88afd63293c78fd6b5fc8ec
Eyub197 <eyubmehmed122@gmail.com>
2026-06-23 18:59:34 +0300

    Got it

--- whimsical-animations/1-particals/3-falling-emoji/src/index.js
+++ whimsical-animations/1-particals/3-falling-emoji/src/index.js
@@ -5,11 +5,22 @@ import "./styles.css";
 const emojiWrapper = document.querySelector(".emojiWrapper");
 const emojiesArray = ["🎉", "👍", "☕", "🎊", "💸"];
 
-emojiesArray.forEach((emoji) => {
+window.setInterval(() => {
 	const emojiEl = document.createElement("div");
+	const emoji = sample(emojiesArray);
+	const animationDuration = `${Math.random() * 2 + 1}`;
+
 	emojiEl.classList.add("emoji");
 	emojiEl.setAttribute("aria-hidden", true);
 	emojiEl.textContent = emoji;
-	emojiEl.style.animationDuration = `${Math.random() * 2 + 1}s`;
+	emojiEl.style.animationDuration = `${animationDuration}s`;
+	emojiEl.style.left = `${Math.random() * 100}%`;
 	emojiWrapper.appendChild(emojiEl);
-});
+
+	window.setTimeout(
+		() => {
+			emojiEl.remove();
+		},
+		animationDuration * 1000 + 200,
+	);
+}, 200);
