dfed1847d7a7d00e9e52c0bf8495b63e7b6edd76
Eyub197 <eyubmehmed122@gmail.com>
2026-06-29 14:17:37 +0300

    Better easing

--- whimsical-animations/1-particals/heart/src/index.js
+++ whimsical-animations/1-particals/heart/src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 
 const btn = document.querySelector(".particleButton");
 
-const FADE_DURATION = 1000;
+const FADE_DURATION = 1100;
 
 btn.addEventListener("click", () => {
 	btn.classList.toggle("liked");
@@ -19,6 +19,7 @@ btn.addEventListener("click", () => {
 	range(5).forEach(() => {
 		const particle = document.createElement("span");
 		particle.classList.add("particle");
+
 		const x = `${random(-48, 48)}px`;
 		const y = `${random(-48, 48)}px`;
 		particle.style.transform = `translate(${x}, ${y})`;
