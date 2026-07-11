befea13a000b18b1a9e417b8f7c269c5dbfb2c7c
Eyub197 <eyubmehmed122@gmail.com>
2026-07-11 17:38:44 +0300

    new utils

--- whimsical-animations/utils.js
+++ whimsical-animations/utils.js
@@ -1,28 +1,57 @@
-export function random(min, max) {
-	return Math.random() * (max - min) + min;
-}
+export const clamp = (value, min = 0, max = 1) => {
+	if (min > max) {
+		[min, max] = [max, min];
+	}
 
-function convertDegreesToRadians(angle) {
-	return (angle * Math.PI) / 180;
-}
+	return Math.max(min, Math.min(max, value));
+};
 
-export function convertPolarToCartesian(angle, distance) {
-	const angleInRadians = convertDegreesToRadians(angle);
-	const x = Math.cos(angleInRadians) * distance;
-	const y = Math.sin(angleInRadians) * distance;
-
-	return [x, y];
-}
-
-export function normalize(
-	number,
-	currentSclaeMin,
-	currentSclaeMax,
+export const normalize = (
+	value,
+	currentScaleMin,
+	currentScaleMax,
 	newScaleMin = 0,
 	newScaleMax = 1,
-) {
-	const standartNomralizatoin =
-		(number - currentSclaeMin) / (currentSclaeMax - currentSclaeMin);
+) => {
+	const standardNormalization =
+		(value - currentScaleMin) / (currentScaleMax - currentScaleMin);
 
-	return (newScaleMax - newScaleMin) * standartNomralizatoin + newScaleMin;
-}
+	return (newScaleMax - newScaleMin) * standardNormalization + newScaleMin;
+};
+
+export const clampedNormalize = (
+	value,
+	currentScaleMin,
+	currentScaleMax,
+	newScaleMin = 0,
+	newScaleMax = 1,
+) => {
+	return clamp(
+		normalize(
+			value,
+			currentScaleMin,
+			currentScaleMax,
+			newScaleMin,
+			newScaleMax,
+		),
+		newScaleMin,
+		newScaleMax,
+	);
+};
+
+// In addition to _linear_ interpolation, I sometimes want to use _exponential_ interpolation, where the input is mapped onto a curved line rather than a straight one. This is beyond the scope of this lesson, but feel free to experiment with this!
+export const exponentialNormalize = (
+	value,
+	currentScaleMin,
+	currentScaleMax,
+	newScaleMin = 0,
+	newScaleMax = 1,
+	exponent = 2,
+) => {
+	const normalizedInput =
+		(value - currentScaleMin) / (currentScaleMax - currentScaleMin);
+
+	const exponentialOutput = Math.pow(normalizedInput, exponent);
+
+	return newScaleMin + (newScaleMax - newScaleMin) * exponentialOutput;
+};
