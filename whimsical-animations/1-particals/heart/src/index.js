51a88c0d2da16e086f01b75f0ce8749ee8d6cb29
Eyub197 <eyubmehmed122@gmail.com>
2026-07-17 13:21:13 +0300

    heat animation made it better

--- whimsical-animations/1-particals/heart/src/index.js
+++ whimsical-animations/1-particals/heart/src/index.js
@@ -1,45 +1,85 @@
-import { range } from "lodash";
-import { random, convertPolarToCartesian } from "../../../utils";
+import { range, random } from "lodash";
+import { normalize } from "../../../utils";
 import "./reset.css";
 import "./styles.css";
-import { normalize } from "../../../utils";
 
 const btn = document.querySelector(".particleButton");
 
-const FADE_DURATION = 1000;
-const NUM_OF_PARTICLES = 5;
-const JITTER = 40;
+const MIN_DISTANCE = 32;
+const MAX_DISTANCE = 64;
+const MIN_FADE_DURATION = 1000 - 500;
+const MAX_FADE_DURATION = 1000 + 500;
+const MAX_FADE_DALAY = 500;
+const MAX_FADE_ADJUST = 500;
+const NUM_OF_PARTICLES = 15;
 
 btn.addEventListener("click", () => {
 	btn.classList.toggle("liked");
-
 	const isLiked = btn.classList.contains("liked");
 	if (!isLiked) {
 		return;
 	}
 
-	const particles = [];
+	const isMotionEnabled = window.matchMedia(
+		"(prefers-reduced-motion: no-preference)",
+	).matches;
 
+	if (!isMotionEnabled) return;
+
+	const particles = [];
 	range(NUM_OF_PARTICLES).forEach((index) => {
 		const particle = document.createElement("span");
 		particle.classList.add("particle");
 
 		let angle = normalize(index, 0, NUM_OF_PARTICLES, 0, 360);
-		angle += random(-JITTER, JITTER);
-		const distance = `${random(32, 64)}`;
+		angle += random(-40, 40);
 
-		const [x, y] = convertPolarToCartesian(angle, distance);
-		particle.style.setProperty("--x", `${x}px`);
-		particle.style.setProperty("--y", `${y}px`);
+		const distance = random(MIN_DISTANCE, MAX_DISTANCE);
 
-		particle.style.setProperty("--fade-duration", `${FADE_DURATION}ms`);
+		particle.style.setProperty("--angle", `${angle}deg`);
+		particle.style.setProperty("--distance", `${distance}px`);
+
+		particle.style.setProperty(
+			"--fade-duration",
+			normalize(
+				distance,
+				MIN_DISTANCE,
+				MAX_DISTANCE,
+				MIN_FADE_DURATION,
+				MAX_FADE_DURATION,
+			) +
+				random(-200, 200) +
+				"ms",
+		);
+
+		particle.style.setProperty(
+			"--fade-delay",
+			normalize(distance, MIN_DISTANCE, MAX_DISTANCE, 0, MAX_FADE_DALAY) +
+				random(0, MAX_FADE_ADJUST) +
+				"ms",
+		);
+
+		particle.style.setProperty(
+			"--pop-duration",
+			normalize(distance, MIN_DISTANCE, MAX_DISTANCE, 400, 800) +
+				random(-200, 200) +
+				"ms",
+		);
+
+		particle.style.setProperty("--size", `${random(9, 15)}px`);
+		particle.style.setProperty("--twinkle-duration", `${random(150, 300)}ms`);
+		particle.style.setProperty("--twinkle-amount", `${random(3.325, 1, true)}`);
+
 		btn.appendChild(particle);
 		particles.push(particle);
 	});
 
-	window.setTimeout(() => {
-		particles.forEach((particle) => {
-			particle.remove();
-		});
-	}, FADE_DURATION + 200);
+	window.setTimeout(
+		() => {
+			particles.forEach((particle) => {
+				particle.remove();
+			});
+		},
+		MAX_FADE_DURATION + MAX_FADE_DALAY + MAX_FADE_ADJUST + 200,
+	);
 });
