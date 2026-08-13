b42174ca62bc251bc44dc57bced2a38c6dfe3fa8
Eyub197 <eyubmehmed122@gmail.com>
2026-08-13 20:32:28 +0300

    first exercise ready

--- whimsical-animations/2-The-Magic-of-SVG/7-Self-drawing-swoop/src/index.js
+++ whimsical-animations/2-The-Magic-of-SVG/7-Self-drawing-swoop/src/index.js
@@ -1,2 +1,6 @@
-import './reset.css';
-import './styles.css';
+import "./reset.css";
+import "./styles.css";
+
+const elem = document.querySelector("path");
+const pathLength = elem.getTotalLength();
+console.log(pathLength);
