805d4fdae7399fa6c07934e1da6e9d87637198b9
Eyub197 <eyubmehmed122@gmail.com>
2026-08-14 10:03:56 +0300

    drawing cyrcle done

--- whimsical-animations/2-The-Magic-of-SVG/8-Circular-progress-indicator/src/index.js
+++ whimsical-animations/2-The-Magic-of-SVG/8-Circular-progress-indicator/src/index.js
@@ -1,15 +1,22 @@
-import './reset.css';
-import './styles.css';
-import { normalize } from './utils.js';
+import "./reset.css";
+import "./styles.css";
+import { normalize } from "./utils.js";
 
-const slider = document.querySelector('#progressSlider');
-const bigNumber = document.querySelector('.bigNumber');
-const progressIndicator = document.querySelector('.progressIndicator');
+const slider = document.querySelector("#progressSlider");
+const bigNumber = document.querySelector(".bigNumber");
+const progressIndicator = document.querySelector(".progressIndicator");
 
 function handleChange(ev) {
-  const progress = Number(ev.target.value);
-  
-  bigNumber.innerText = progress + '%';
+	const progress = Number(ev.target.value);
+
+	bigNumber.innerText = progress + "%";
+	progressIndicator.style.strokeDashoffset = normalize(
+		progress,
+		0,
+		100,
+		100,
+		0,
+	);
 }
 
-slider.addEventListener('input', handleChange);
+slider.addEventListener("input", handleChange);
