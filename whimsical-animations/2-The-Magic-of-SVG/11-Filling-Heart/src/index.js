79479e8a4f0e800239979601bed11d0794b25255
Eyub197 <eyubmehmed122@gmail.com>
2026-08-16 09:58:13 +0300

    2 exercises left

--- whimsical-animations/2-The-Magic-of-SVG/11-Filling-Heart/src/index.js
+++ whimsical-animations/2-The-Magic-of-SVG/11-Filling-Heart/src/index.js
@@ -1,25 +1,30 @@
-import { normalize } from './utils';
-import './reset.css';
-import './styles.css';
+import { normalize } from "./utils";
+import "./reset.css";
+import "./styles.css";
 
-const btn = document.querySelector('.like-btn');
+const btn = document.querySelector(".like-btn");
 
 const MAX_NUM_OF_LIKES = 10;
 let numOfLikes = 0;
 
-btn.addEventListener('click', () => {
-  numOfLikes += 1;
+btn.addEventListener("click", () => {
+	numOfLikes += 1;
 
-  // When the button is fully liked and the user clicks,
-  // reset back to zero. This is done purely to make
-  // development easier, this isn’t really a feature
-  if (numOfLikes > MAX_NUM_OF_LIKES) {
-    numOfLikes = 0;
-  }
+	// When the button is fully liked and the user clicks,
+	// reset back to zero. This is done purely to make
+	// development easier, this isn’t really a feature
+	if (numOfLikes > MAX_NUM_OF_LIKES) {
+		numOfLikes = 0;
+	}
 
-  const fillRatio = numOfLikes / MAX_NUM_OF_LIKES;
-  
-  // TODO: use `fillRatio` to control the perceived
-  // fill amount.
-  // (0 = empty, 1 = full)
+	const fillRatio = numOfLikes / MAX_NUM_OF_LIKES;
+	const fillAmount = normalize(fillRatio, 0, 1, 21.8, 3.9);
+
+	const heartFill = document.querySelector(".heart-fill");
+	heartFill.setAttribute("y", fillAmount);
+
+	//
+	// TODO: use `fillRatio` to control the perceived
+	// fill amount.
+	// (0 = empty, 1 = full)
 });
