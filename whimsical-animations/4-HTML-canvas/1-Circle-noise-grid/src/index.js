470ac5472013010f50a2b5f13bd166679a0393c4
Eyub197 <eyubmehmed122@gmail.com>
2026-09-02 19:35:05 +0300

    first canvas exercise

--- whimsical-animations/4-HTML-canvas/1-Circle-noise-grid/src/index.js
+++ whimsical-animations/4-HTML-canvas/1-Circle-noise-grid/src/index.js
@@ -1,10 +1,12 @@
-import { random, range } from 'lodash';
-import { setupCanvas, normalize } from './utils';
-import './reset.css';
-import './styles.css';
+import { random, range } from "lodash";
+import { normalize, setupCanvas } from "./utils";
+import "./reset.css";
+import "./styles.css";
 
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 const { ctx, canvasWidth, canvasHeight } = setupCanvas(canvas);
+const NUMBER_OF_ROWS = 20;
+const NUMBER_OF_COLUMNS = 20;
 
 // Acceptance Criteria:
 // - A 20-by-20 grid of circles
@@ -12,7 +14,19 @@ const { ctx, canvasWidth, canvasHeight } = setupCanvas(canvas);
 //   hsl(210deg 15% 6%) to hsl(210deg 15% 100%).
 
 function draw() {
-  // TODO
+	range(NUMBER_OF_ROWS).forEach((row) => {
+		range(NUMBER_OF_COLUMNS).forEach((column) => {
+			const cellSize = canvasWidth / NUMBER_OF_COLUMNS;
+			const radius = cellSize / 2;
+			ctx.beginPath();
+			const x = normalize(row, 0, NUMBER_OF_ROWS, 0, canvasWidth) + radius;
+			const y =
+				normalize(column, 0, NUMBER_OF_COLUMNS, 0, canvasHeight) + radius;
+			ctx.arc(x, y, radius, 0, Math.PI * 2);
+			ctx.fillStyle = `hsl(210deg 15% ${random(6, 100)}%)`;
+			ctx.fill();
+		});
+	});
 }
 
 draw();
