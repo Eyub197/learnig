17d53f772a983fa1a464932b8071354257fb2d0e
Eyub197 <eyubmehmed122@gmail.com>
2026-09-04 19:05:07 +0300

    refactored

--- whimsical-animations/4-HTML-canvas/2-DVD-logo/src/index.js
+++ whimsical-animations/4-HTML-canvas/2-DVD-logo/src/index.js
@@ -10,32 +10,29 @@ const img = new Image();
 img.addEventListener("load", draw);
 img.src = DVD_URI;
 
-const INITIAL_VELOCITY_Y = 1;
-const INITIAL_VELOCITY_X = 1;
-
 const logo = {
 	width: 200,
 	height: 88,
 	x: 0,
 	y: 0,
-	velocityY: INITIAL_VELOCITY_Y,
-	velocityX: INITIAL_VELOCITY_X,
+	velocityY: 1,
+	velocityX: 1,
 };
 
 function draw() {
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 	ctx.drawImage(img, logo.x, logo.y, logo.width, logo.height);
 
-	if (logo.y > canvasHeight - logo.height) {
-		logo.velocityY = INITIAL_VELOCITY_Y * -1;
-	} else if (logo.y < 0) {
-		logo.velocityY = INITIAL_VELOCITY_Y;
-	}
+	const isLogoBeyondBoundsVertically =
+		logo.y > canvasHeight - logo.height || logo.y < 0;
+	const isLogoBeyondBoundsHorizontally =
+		logo.x > canvasWidth - logo.width || logo.x < 0;
 
-	if (logo.x > canvasWidth - logo.width) {
-		logo.velocityX = INITIAL_VELOCITY_X * -1;
-	} else if (logo.x < 0) {
-		logo.velocityX = INITIAL_VELOCITY_X;
+	if (isLogoBeyondBoundsVertically) {
+		logo.velocityY *= -1;
+	}
+	if (isLogoBeyondBoundsHorizontally) {
+		logo.velocityX *= -1;
 	}
 
 	logo.y += logo.velocityY;
